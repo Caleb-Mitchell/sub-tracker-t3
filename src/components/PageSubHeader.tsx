de33fe9cf19ce430064963f74573957a3366dbf0
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-30 12:06:44 -0500

    Center SubHeader Text

--- src/components/PageSubHeader.tsx
+++ src/components/PageSubHeader.tsx
@@ -1,7 +1,7 @@
 export function PageSubHeader() {
   return (
     <>
-      <p className="text-xl text-slate-200">
+      <p className="text-center text-xl text-slate-200">
         A simple app to manage substitute musician information
       </p>
       <hr className="my-5 w-full max-w-xs" />
