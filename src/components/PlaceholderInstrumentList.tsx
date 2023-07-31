444b902240e740528f3c66dd9edbf6fa22c51e1f
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-31 15:25:21 -0500

    Fix styling on instrument list reloads with placeholder list items

---
+++ src/components/PlaceholderInstrumentList.tsx
@@ -0,0 +1,11 @@
+export function PlaceholderInstrumentList() {
+  return (
+    <>
+      <li className="mb-1 ml-8 flex w-full text-lg">...</li>
+      <li className="mb-1 ml-8 flex w-full text-lg">...</li>
+      <li className="mb-1 ml-8 flex w-full text-lg">...</li>
+      <li className="mb-1 ml-8 flex w-full text-lg">...</li>
+      <li className="mb-1 ml-8 flex w-full text-lg">...</li>
+    </>
+  );
+}
