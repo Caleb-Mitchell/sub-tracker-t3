3e3c68e35c54e6af5fb31a4de311fb54a43e2a26
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-08-11 13:54:51 -0500

    Remove extraneous comments

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -62,41 +62,20 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Demo Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: { label: "Demo", type: "text", value: "demo" },
         password: { label: "Password", type: "password", value: "demo" },
       },
       authorize() {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
         const user: User = {
           id: "1",
           name: "Demo",
           email: "Demo",
           emailVerified: new Date(),
-          image: "https://example.com/jsmith.png",
+          image: "https://example.com/demo.png",
         };
-        // make an object of type User
-        // const user = await prisma.user.findUnique({
-        //   where: { email: credentials.email },
-        // });
-
-        // If no error and we have user data, return it
-        if (user) {
-          return user;
-        }
-        // Return null if user data could not be retrieved
-        return null;
+        return user ? user : null;
       },
     }),
     /**
