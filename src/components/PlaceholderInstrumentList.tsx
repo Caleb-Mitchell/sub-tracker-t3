f91972808a018d6eab3331f750d812f6b110c58a
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-08-14 12:38:03 -0500

    Refactor to make more DRY

--- src/components/PlaceholderInstrumentList.tsx
+++ src/components/PlaceholderInstrumentList.tsx
@@ -1,11 +1,9 @@
 export function PlaceholderInstrumentList() {
-  return (
-    <>
-      <li className="mb-2 flex w-full px-3 text-lg"></li>
-      <li className="mb-2 flex w-full px-3 text-lg"></li>
-      <li className="mb-2 flex w-full px-3 text-lg"></li>
-      <li className="mb-2 flex w-full px-3 text-lg"></li>
-      <li className="mb-2 flex w-full px-3 text-lg"></li>
-    </>
-  );
+  const rows = [];
+  for (let i = 0; i < 5; i++) {
+    rows.push(
+      <li className="mb-2 flex w-full px-3 text-lg">&#8226;&#8226;&#8226;</li>
+    );
+  }
+  return <div className="animate-pulse">{rows}</div>;
 }
