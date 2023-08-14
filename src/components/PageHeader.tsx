83f8984fe098c3db35bf6373e910e5e932be54ea
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-08-14 12:48:15 -0500

    Add link to home page from header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export function PageHeader() {
   return (
     <header className="flex flex-col items-center text-center">
-      <h1 className="text-4xl text-slate-200">Sub Tracker</h1>
+      <Link href="/instruments">
+        <h1 className="text-4xl text-slate-200">Sub Tracker</h1>
+      </Link>
     </header>
   );
 }
